6249b47032c999b84a09cfb6444e60209a57a8a9
alexisbernt <129408149+alexisbernt@users.noreply.github.com>
2025-09-07 15:50:09 -0500

    start of AI page /layout

--- lexi-portfolio/src/pages/AI.js
+++ lexi-portfolio/src/pages/AI.js
@@ -1,7 +1,28 @@
-function AI() {
-  return <h2>Lexi likes projects and adventures. Check this out: 
+import "../css/AI.css";
 
-    donate to the fund
-  </h2>;
+function AI() {
+  return (
+    <div className="ai-container">
+      <div className="ai-grid">
+        <div className="ai-box">
+          <h3>Box 1 header</h3>
+          <p>Box 1 body</p>
+        </div>
+        <div className="ai-box">
+          <h3>Box box box</h3>
+          <p>box</p>
+        </div>
+        <div className="ai-box">
+          <h3>header</h3>
+          <p>body</p>
+        </div>
+        <div className="ai-box">
+          <h3>Ice cream</h3>
+          <p>read</p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default AI;
