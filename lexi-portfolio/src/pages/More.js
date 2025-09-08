1e735943ac5fb2705f8cba51da2e00581fc7cdaf
alexisbernt <129408149+alexisbernt@users.noreply.github.com>
2025-09-07 22:47:41 -0500

    Added More page

--- lexi-portfolio/src/pages/More.js
+++ lexi-portfolio/src/pages/More.js
@@ -1,4 +1,49 @@
+import '../css/More.css';
+
 function More() {
-  return <h2>More Page</h2>;
+  return (
+    <div className="more-container">
+      <div className="more-grid">
+        <div className="more-box">
+          <h3>Professional Connections</h3>
+          <p>
+            <a 
+              href="https://www.linkedin.com/in/alexis-bernthal" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+             Connect with me!
+            </a>
+          </p>
+        </div>
+        <div className="more-box">
+          <h3>Social Connections</h3>
+          <p>
+            <a 
+              href="https://www.instagram.com/alexis.wink/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              A REELy cool world.
+            </a>
+          </p>
+        </div>
+        <div className="more-box">
+          <h3>Learnings in video form</h3>
+          <p>
+            <a 
+              href="https://www.youtube.com/@lexicyber" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              One of the videos seems to be popular...
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default More;
+
